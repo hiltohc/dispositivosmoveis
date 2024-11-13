00ea0b7176f6ead3a1534b8b80cbc819e249f727
J HILTO DO NASCIMENTO <105225792+hiltohc@users.noreply.github.com>
2024-11-12 23:52:21 -0300

    atualização final

---
+++ my-app/NotificationConfig.js
@@ -0,0 +1,10 @@
+// NotificationConfig.js
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.configure({
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios'
+});
