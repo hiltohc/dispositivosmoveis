b163e51613c69732366e280fc3cb7209ad6e54ea
J HILTO DO NASCIMENTO <105225792+hiltohc@users.noreply.github.com>
2024-11-29 16:05:12 -0300

    Update firebaseConnection.js

--- my-app/src/firebaseConnection.js
+++ my-app/src/firebaseConnection.js
@@ -5,12 +5,12 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDa5DldqRm-uFlZmTEaFI0ojYEHt_BaLdg",
-  authDomain: "projscs-2024.firebaseapp.com",
-  projectId: "projscs-2024",
-  storageBucket: "projscs-2024.firebasestorage.app",
-  messagingSenderId: "209077765839",
-  appId: "1:209077765839:web:b82e719a6f2f42d12199ba"
+  apiKey: " ", // coloque os dados do projeto do firebase
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " "
 };
 
 
